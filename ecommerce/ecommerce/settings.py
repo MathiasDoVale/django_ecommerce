019c6d48b20c5c12123f99e5a72d1cac3b101843
mathido <matdovale@gmail.com>
2023-12-15 17:58:21 -0300

    Adding Cors Config to accept localhost:3000 calls

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -161,3 +163,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
